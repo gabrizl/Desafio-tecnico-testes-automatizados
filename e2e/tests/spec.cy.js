8b0abfa1acde00355a39a439262b4c74209807c5
gabrizl <gsilva.pb@gmail.com>
2022-06-17 14:53:53 -0300

    second update

--- e2e/tests/spec.cy.js
+++ e2e/tests/spec.cy.js
@@ -48,7 +48,7 @@ describe("authentication", () => {
     logIn.loginButton().click();
   });
 
-  it.only("buy a casual dress logged in site", () => {
+  it("buy a casual dress logged in site", () => {
     const create = new CreateAccount();
     const logIn = new login();
     const dress = new dresses();
@@ -71,39 +71,8 @@ describe("authentication", () => {
 
   });
 
-  it("buy casual dress directly", () => {
+  it.only("buy casual dress directly", () => {
    
   });
 
-  it("can filter for completed tasks", () => {
-    // We can perform similar steps as the test above to ensure
-    // that only completed tasks are shown
-    cy.contains("Completed").click();
-
-    cy.get(".todo-list li")
-      .should("have.length", 1)
-      .first()
-      .should("have.text", "Pay electric bill");
-
-    cy.contains("Walk the dog").should("not.exist");
-  });
-
-  it("can delete all completed tasks", () => {
-    // First, let's click the "Clear completed" button
-    // `contains` is actually serving two purposes here.
-    // First, it's ensuring that the button exists within the dom.
-    // This button only appears when at least one task is checked
-    // so this command is implicitly verifying that it does exist.
-    // Second, it selects the button so we can click it.
-    cy.contains("Clear completed").click();
-
-    // Then we can make sure that there is only one element
-    // in the list and our element does not exist
-    cy.get(".todo-list li")
-      .should("have.length", 1)
-      .should("not.have.text", "Pay electric bill");
-
-    // Finally, make sure that the clear button no longer exists.
-    cy.contains("Clear completed").should("not.exist");
-  });
 });
